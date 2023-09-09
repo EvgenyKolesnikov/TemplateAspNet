75c9de85852a1c93e5f83e6cfcdad7566f3d4f16
jenya <jenek888q@gmail.com>
2023-09-10 00:48:51 +0300

    Init

---
+++ TemplateAspNet/Controllers/MainController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TemplateAspNet.Database;
+using TemplateAspNet.Model;
+
+namespace TemplateAspNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase
+    {
+        public readonly MainDbContext _dbContext;
+
+        public MainController(MainDbContext DbContext) 
+        {
+            _dbContext = DbContext;
+        }
+
+        [HttpGet("Test")]
+        public async Task<List<Product>> test()
+        {
+            await _dbContext.Products.AddAsync(new Product() {Name = "Banana", Description = "Yellow banana", Price = 100 });
+            await _dbContext.SaveChangesAsync();
+
+            var products = await _dbContext.Products.ToListAsync();
+            return products;
+        }
+
+        [HttpGet("Test1")]
+        public async Task<string> test1()
+        {
+            return "Hello World";
+        }
+
+    }
+}
---
+++ TemplateAspNet/Database/MainDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Emit;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using TemplateAspNet.Model;
+
+namespace TemplateAspNet.Database
+{
+    public class MainDbContext : DbContext
+    {
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options) 
+        { 
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Assembly assemblyWithConfigurations = GetType().Assembly; //get whatever assembly you want
+            modelBuilder.ApplyConfigurationsFromAssembly(assemblyWithConfigurations);
+        }
+    }
+}
